b59c0f2e0126a6cb9cb7664017596133b03cd669
chiwaka <41538823+chiwaka@users.noreply.github.com>
2018-10-13 12:05:00 +0200

    jose

--- js/js.js
+++ js/js.js
@@ -264,6 +264,7 @@ function inicializar() {
 		Usuario.token=data.registrationId;
 	});
 	push.on('notification', (data) => {
+		navigator.vibrate
 		swal({
 			//type: "question",
 			padding:"10px",
